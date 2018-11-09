e7803f5f0d2082b001be3f66c46d28998753ad71
Yassine Benrazougui <yassine.benrazougui@sts.ch>
2018-11-09 15:42:50 +0100

    Adding boostrap
    Add bootstrap local files inside my project website

--- index.php
+++ index.php
@@ -3,9 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <title>Site Web | Yassine Benrazougui</title>
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="css/bootstrap/bootstrap.css">
+    <script type="text/javascript" src="css/bootsrap/bootstrap.css"></script>
 </head>
 
 <body>
-
+<?php
+    echo 'Hello world!';
+?>
 </body>
 </html>
